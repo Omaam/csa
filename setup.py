2c0fb94e3962658ad2e237825e68688125391574
Tomoki Omama <tomama0920@gmail.com>
2021-01-06 22:24:02 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csa',
+    version='0.0.1',
+    description="common-signal analysis tool",
+    long_description="",
+    author='Tomoki Omama',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    packages=['csa']
+)
