3b0165951292a4199d2f8fb946b5e389979d2179
Tomoki Omama <tomama0920@gmail.com>
2021-01-06 21:49:32 +0900

    make csa directory

--- example/_stft_test.py
+++ example/_stft_test.py
@@ -1,13 +1,18 @@
+import sys
+sys.path.append('..')
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
 
+from window_function import WindowGenerator
+
 def main():
     # setting
     infile1 = 'xdata.dat'
     infile2 = 'odata.dat'
-    freqinfo = [0, 0.5, 10000]
+    freqinfo = [0, 0.5, 2000]
     lambdainfo = [1e-1, 1e2, 20]
     nsegment = 1000
     noverlap = 500
@@ -18,6 +23,24 @@ def main():
     time = xdata[:,0]
     x = xdata[:,1]
     o = odata[:,1]
+
+    # figure
+    t = xdata[:1000, 0]
+    y = xdata[:1000, 1]
+    y_d = y - y.mean()
+    window = WindowGenerator((t[0], t[-1]))
+    window.hann()
+    y_w = window.gene(t) * y
+    y_dw = window.gene(t) * y_d
+    y_adw = window.gene(t) * y_d + y.mean()
+    plt.plot(t, y, t, y_w, t, y_dw, t, y_adw)
+    plt.legend(['Original', 'Filter(original)',
+                'Filter(deviation)', 'Ave. + Filter(deviation)'])
+    plt.xlabel('time')
+    plt.ylabel('flux')
+    plt.tight_layout()
+    plt.savefig('example_ori_vs_win.png')
+    plt.show()
 
     f, t, Zxx_x = signal.stft(xdata[:,1], 1,
                               nperseg=nsegment, noverlap=noverlap)
@@ -43,7 +66,7 @@ def main():
     _, xrec = signal.istft(Zxx_x, 1)
     plt.figure()
     plt.plot(_, xrec, time, x, time, o)
-    plt.xlim(0, 500)
+    # plt.xlim(0, 1000)
     plt.xlabel('Time [sec]')
     plt.ylabel('Signal')
     plt.legend(['Filtered via STFT', 'True Carrier'])
